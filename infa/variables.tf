2fc967b557b5ff8742c19e031fac90409ce5cd84
Deion Davis <deion.davis@slalom.com>
2022-09-15 12:01:37 -0400

    Added initial terraform scripts

---
+++ infa/variables.tf
@@ -0,0 +1,44 @@
+# Replace default with your aws cli username ex.default
+variable "cliProfile" {
+  type    = string
+  default = "acg_sand"
+}
+
+# Replace with name for tagging ex. deion.davis
+variable "yourNameTag" {
+  type    = string
+  default = "deion.davis"
+}
+
+# name of your key file
+variable "keyName" {
+  type    = string
+  default = "dedeKeyPair"
+}
+
+# Added your name -securityGroup is appended to the end of whatever you input
+variable "securityGroupName" {
+  type    = string
+  default = "Dede"
+}
+
+#input your ip address so that you can ssh into the ec2 insance
+variable "ipAddresses" {
+  type    = list
+  default = ["184.57.36.105/32", "23.120.103.1/32"]
+}
+# input name of your userdatafile
+variable "userData" {
+  type    = string
+  default = "TestfileName"
+}
+/*
+variable "keyContent" {
+  type = string
+  default = "${file("${path.cwd}/public_DD_KeyPair.txt")}"
+}
+*/
+variable "targetRegion" {
+  type = string
+  default = "us-east-1"
+}

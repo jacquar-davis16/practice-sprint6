e6d7d9579b49372fcbdaa7f9ec621b28977ba879
Deion Davis <deion.davis@slalom.com>
2022-09-22 15:45:12 -0400

    Updating start_server script

--- api/scripts/start_server.sh
+++ api/scripts/start_server.sh
@@ -3,3 +3,5 @@
 #java -jar /home/ubuntu/api/target/webApp.war
 cd /home/ubuntu/target
 nohup java -jar *.war </dev/null &>/dev/null &
+
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
